a5f65ce315710b6bf817467a2f5fffbf0cf0e97b
projectshokunin <projectshokunin@gmail.com>
2026-05-13 01:12:53 +0000

    Deploying to gh-pages from @ projectshokunin/projectshokunin@b3c8e1c97bd1929cb4115ff08932fe192f357d09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projectshokunin/projects/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projectshokunin/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
